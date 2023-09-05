20e6ceaeeb82ad02f186870fef626bbb3a16dc46
HKalay <hamdullahkalay@Hamdullah-MBP-2.home>
2023-09-06 01:16:09 +0300

    Added some libs

---
+++ app/src/main/java/com/example/bitcointicker/PermissionExtensions.kt
@@ -0,0 +1,19 @@
+package com.example.bitcointicker
+
+import android.Manifest
+import android.os.Build
+import androidx.fragment.app.FragmentActivity
+import com.fondesa.kpermissions.allGranted
+import com.fondesa.kpermissions.coroutines.sendSuspend
+import com.fondesa.kpermissions.extension.permissionsBuilder
+
+suspend fun FragmentActivity.permissionPostNotificationAllGranted(): Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        permissionsBuilder(
+            Manifest.permission.POST_NOTIFICATIONS
+        ).build()
+            .sendSuspend().allGranted()
+    } else {
+        return true
+    }
+}
