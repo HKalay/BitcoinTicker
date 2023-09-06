d814269ae17f89f07e2f2d9dc2427f6fc0b97545
HKalay <hamdullahkalay@Hamdullah-MacBook-Pro-2.local>
2023-09-06 16:12:19 +0300

    .

--- app/src/main/java/com/example/bitcointicker/app/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/bitcointicker/app/ui/splash/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.example.bitcointicker.app.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.bitcointicker.R
+import com.example.bitcointicker.app.base.BaseActivity
+import com.example.bitcointicker.app.ui.HomeActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity(R.layout.activity_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        startActivity()
+    }
+
+    private fun startActivity() {
+        Handler().postDelayed({
+            //TODO daha önce giriş yapmışsa home page gidecek. Yapmamışsa login pagede olacak.
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 2000)
+
     }
 }
