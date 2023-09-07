814718a373c1dcd440803defc56d72b58b32f29b
HKalay <hamdullahkalay@Hamdullah-MBP-2.home>
2023-09-07 12:48:07 +0300

    refactor svg

--- app/src/main/java/com/example/bitcointicker/app/ui/activity/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/bitcointicker/app/ui/activity/signup/SignUpActivity.kt
@@ -3,7 +3,11 @@ package com.example.bitcointicker.app.ui.activity.signup
 import android.os.Bundle
 import com.example.bitcointicker.R
 import com.example.bitcointicker.app.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_login.etPassword
 import kotlinx.android.synthetic.main.activity_sign_up.btnSignUpCancel
+import kotlinx.android.synthetic.main.activity_sign_up.etPasswordAgain
+import kotlinx.android.synthetic.main.activity_sign_up.imgShowHidePasswordAgainSignUp
+import kotlinx.android.synthetic.main.activity_sign_up.imgShowHidePasswordSignUp
 
 class SignUpActivity : BaseActivity(R.layout.activity_sign_up) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +26,30 @@ class SignUpActivity : BaseActivity(R.layout.activity_sign_up) {
         btnSignUpCancel.setOnClickListener {
             finish()
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }
+
+        imgShowHidePasswordSignUp.setOnClickListener {
+
+            //TODO burası düzenlecenecek resim değişmiyor.
+            imgShowHidePasswordSignUp.isSelected = !imgShowHidePasswordSignUp.isSelected
+            val inputType = if (imgShowHidePasswordSignUp.isSelected) {
+                129
+            } else {
+                128
+            }
+            etPassword.inputType = inputType
+        }
+
+        imgShowHidePasswordAgainSignUp.setOnClickListener {
+
+            //TODO burası düzenlecenecek resim değişmiyor.
+            imgShowHidePasswordAgainSignUp.isSelected = !imgShowHidePasswordAgainSignUp.isSelected
+            val inputType = if (imgShowHidePasswordAgainSignUp.isSelected) {
+                129
+            } else {
+                128
+            }
+            etPasswordAgain.inputType = inputType
         }
     }
 }
