5e38386b84aacf52d2a7823522a1f9769620d24d
HKalay <hamdullahkalay@Hamdullah-MacBook-Pro-2.local>
2023-09-07 15:22:32 +0300

    refactor code

--- app/src/main/java/com/example/bitcointicker/app/ui/activity/signup/SignUpSuccessActivity.kt
+++ app/src/main/java/com/example/bitcointicker/app/ui/activity/signup/SignUpSuccessActivity.kt
@@ -1,10 +1,8 @@
 package com.example.bitcointicker.app.ui.activity.signup
 
-import android.content.Intent
 import android.os.Bundle
 import com.example.bitcointicker.R
 import com.example.bitcointicker.app.base.BaseActivity
-import com.example.bitcointicker.app.ui.activity.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_sign_up_success.llGoLoginPage
 
 class SignUpSuccessActivity : BaseActivity(R.layout.activity_sign_up_success) {
@@ -15,8 +13,6 @@ class SignUpSuccessActivity : BaseActivity(R.layout.activity_sign_up_success) {
 
     private fun initClickListener(){
         llGoLoginPage.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
-
             overridePendingTransition(
                 R.anim.fade_in,
                 R.anim.fade_out
